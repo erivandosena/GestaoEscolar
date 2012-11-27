170bd5190e90847ab4af41a411594e1a600eeda6
Erivando Sena <erivandosena@gmail.com>
2012-11-27 13:41:53 +0000

    Importação inicial do sistema sisescola em Java SE e Maven.

---
+++ src/main/java/br/net/rwd/sisescola/App.java
@@ -0,0 +1,18 @@
+package br.net.rwd.sisescola;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!\nEm 26/11/2012 tudo se iniciou aqui!" );
+        JFrame f=new JFrame("Sistema de gerenciamento escolar");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
